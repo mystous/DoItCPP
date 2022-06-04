cc2318ea017b5620d86194649e4697cda797bdae
DesignMoon <enjoymoon@gmail.com>
2022-06-05 00:08:45 +0900

    2장 예제
    부동 소수점 출력해보기

---
+++ sample_code/ch02/print_floating_point/print_floating_point.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << 9.87654321f << endl;
+	cout << 987654.321f << endl;
+	cout << 9876543.21f << endl;
+	cout << 0.00000987654321f << endl;
+	cout << 0.00000000000987654321f << endl;
+
+	return 0;
+}
