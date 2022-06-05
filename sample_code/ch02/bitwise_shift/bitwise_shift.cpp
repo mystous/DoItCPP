881595671b1d913672ff2033771fa9a74bcee313
DesignMoon <enjoymoon@gmail.com>
2022-06-05 16:21:29 +0900

    2장 예제
    bitwise Shift (a >> b, a << b)

---
+++ sample_code/ch02/bitwise_shift/bitwise_shift.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+    int a = 13;
+    int b = a >> 1;     //1 bit Shift Right 
+    int c = a << 1;     //1 bit Shift Left  
+    int d = a >> -1;    //shift 수행 오류 (warning C4293)
+    int e = a << 32;    //shift 수행 오류 (warning C4293)
+
+    cout << "a = " << bitset<8>(a) << " : " << a << endl;
+    cout << "b = " << bitset<8>(b) << " : " << b << endl;
+    cout << "c = " << bitset<8>(c) << " : " << c << endl;
+    cout << "d = " << bitset<8>(d) << " : " << d << endl;
+    cout << "e = " << bitset<8>(e) << " : " << e << endl;
+
+    return 0;
+}
