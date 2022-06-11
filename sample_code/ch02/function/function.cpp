571c1a1072fa49c767ed953a74e49c6fccd5bcfb
DesignMoon <enjoymoon@gmail.com>
2022-06-11 11:38:33 +0900

    2장 예제
    덧셈을 수행하는 함수 add

---
+++ sample_code/ch02/function/function.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int add(int _x, int _y) //add 함수 정의
+{
+    int result = _x + _y;
+    return result;
+}
+
+int main()
+{
+    int add_result = add(2, 3); //add 함수 호출
+
+    cout << "add 함수 결과: " << add_result << endl;  //함수 결과 출력
+
+    return 0;
+}
