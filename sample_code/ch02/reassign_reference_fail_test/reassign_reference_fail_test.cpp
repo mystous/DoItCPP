546884863eda1d741f3bb2060fe654bffab95b51
DesignMoon <enjoymoon@gmail.com>
2022-06-12 09:50:10 +0900

    2장 예제
    레퍼런스 변수 재지정은 불가 테스트

---
+++ sample_code/ch02/reassign_reference_fail_test/reassign_reference_fail_test.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int value = 10;
+    int& ref_value = value;
+
+    int value2 = 20;
+    ref_value = value2; //레퍼런스 재지정을 의도했으나, 의도와 다르게 동작하는 부분
+
+    cout << "value: " << value << endl;
+    cout << "ref_value: " << ref_value << endl;
+
+    return 0;
+}
