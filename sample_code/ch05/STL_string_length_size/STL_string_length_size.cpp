3966bf3507f1041cfd0301553520019dbe939c68
DesignMoon <enjoymoon@gmail.com>
2022-10-16 10:13:55 +0900

    5장 string length 와 size
    5장 string length 와 size

---
+++ sample_code/ch05/STL_string_length_size/STL_string_length_size.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string str1("Hello");
+  cout << str1 << endl;
+
+  cout << str1.length() << endl;
+  cout << str1.size() << endl;
+
+  return 0;
+}
