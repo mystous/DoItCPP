2dccde15a70ef71c4b7742a434298997c387fe50
DesignMoon <enjoymoon@gmail.com>
2022-10-16 19:04:22 +0900

    5장 ==, != 연산자로 문자열 동일 체크 예제
    ==, != 연산자로 문자열 동일 체크 예제

---
+++ sample_code/ch05/STL_string_operator/STL_string_operator.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string s1 = "Hello";
+  string s2 = "Hello";
+  if (s1 == s2) {
+    cout << "두 문자열 일치" << endl;
+  }
+
+  s1 = "Hello";
+  s2 = "World";
+  if (s1 != s2) {
+    cout << "두 문자열 불일치" << endl;
+  }
+
+  return 0;
+}
