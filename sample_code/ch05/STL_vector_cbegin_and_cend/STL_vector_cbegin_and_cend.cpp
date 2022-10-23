8d113dba76c2eb3125a37abe9b1747be2db48d0d
DesignMoon <enjoymoon@gmail.com>
2022-10-23 21:35:42 +0900

    5장 std::vector의 cbegin, cend
    std::vector의 cbegin, cend

---
+++ sample_code/ch05/STL_vector_cbegin_and_cend/STL_vector_cbegin_and_cend.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> vec;
+  vec.push_back(0);
+  vec.push_back(10);
+  vec.push_back(20);
+  vec.push_back(30);
+  vec.push_back(40);
+
+  vector<int>::const_iterator const_it = vec.cbegin();
+  cout << *const_it << endl;
+
+  ++const_it;
+  cout << *const_it << endl;
+
+  const_it = vec.cend();
+  --const_it;
+  cout << *const_it << endl;
+}
