dd0efd0478dd48637ea08f3e5a4f7f28263972c4
조규남 <mystous@gmail.com>
2023-08-06 21:48:33 +0900

    중첩 클래스 템플릿 예제 업데이트

--- sample_code/ch05/class_template_nested_template/class_template_nested_template.cpp
+++ sample_code/ch05/class_template_nested_template/class_template_nested_template.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-template <typename T>
-T data_sum(T operand1, T operand2) {
-  return operand1 + operand2;
-}
+template <typename Type1, typename Type2>
+class data_package {
+public:
+  template<typename Type2>
+  class nested_class_data_package {
+  public:
+    nested_class_data_package(Type2 data) : nested_class_data(data) {}
+    Type2 get_element() { return nested_class_data; }
+    Type2 nested_class_data;
+  };
 
-template <>
-double data_sum(double operand1, double operand2) {
-  return  (int)operand1 + (int)operand2;
-}
+  template<typename Type3>
+  class nested_class {
+  public:
+    nested_class(Type3 data) : nested_class_data(data) {}
+    void print_out_element() {
+      cout << "중첩 클래스 데이터: " << nested_class_data << endl;
+    }
+  private:
+    Type3 nested_class_data;
+  };
 
+  data_package(Type1 first, Type2 second) : first(first), second(second),
+    internal_data(second) {}
+
+  void print_out_element() {
+    cout << "첫 번째 요소: " << first <<
+      ", 두 번째 요소: " << second << endl;
+    cout << "중첩 클래스 요소: " << internal_data.get_element() << endl;
+  }
+
+private:
+  Type1 first;
+  Type2 second;
+  nested_class_data_package<Type2> internal_data;
+
+};
 int main() {
-  int data1 = 3, data2 = 5;
-  double data3 = 4.5, data4 = 8.9;
-  string data5 = "Hello, ", data6 = "World!";
+  data_package<string, int> template_inst1("문자열", 10);
+  data_package<string, int>::nested_class<int> template_inst2(500);
 
-  cout << "정수형 데이터 합: " << data_sum(data1, data2) << endl;
-  cout << "실수형 데이터 합: " << data_sum(data3, data4) << endl;
-  cout << "문자열 데이터 합: " << data_sum(data5, data6) << endl;
+  cout << "중첩 클래스 첫 번째 범례" << endl;
+  template_inst1.print_out_element();
 
-  return 0;
+  cout << endl << "중첩 클래스 두 번째 범례" << endl;
+    template_inst2.print_out_element();
 }
