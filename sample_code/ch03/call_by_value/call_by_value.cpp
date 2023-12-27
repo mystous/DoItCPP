fa79e03579ae020ca4c9413805109e8a8323144b
DesignMoon <enjoymoon@gmail.com>
2023-12-27 12:58:24 +0900

    ch03 레퍼런스 절 예제들

---
+++ sample_code/ch03/call_by_value/call_by_value.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void swap(int a, int b) {
+  // swap 함수 내 교환 전 a, b 값
+  cout << "[swap func] before swap, a: " << a << "  b: " << b << endl;
+
+  int temp = a;
+  a = b;
+  b = temp;
+
+  // swap 함수 내 교환 후 a, b 값
+  cout << "[swap func] after swap, a: " << a << "  b: " << b << endl;
+}
+
+int main() {
+  int a = 5;
+  int b = 10;
+
+  // swap 함수 호출 전 a, b 변수 값
+  cout << "[main] before swap, a: " << a << "  b: " << b << endl << endl;
+
+  swap(a, b);
+
+  // swap 함수 호출 후 a, b 변수 값
+  cout << endl << "[main] after swap, a: " << a << "  b: " << b << endl;
+
+  return 0;
+}
---
+++ sample_code/ch03/reassign_reference_fail_test/reassign_reference_fail_test.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+  int value = 10;
+  int& ref_value = value;
+
+  int value2 = 20;
+  ref_value = value2;   // 레퍼런스 재지정, 의도와 다르게 동작
+
+  cout << "value: " << value << endl;
+  cout << "ref_value: " << ref_value << endl;
+
+  return 0;
+}
