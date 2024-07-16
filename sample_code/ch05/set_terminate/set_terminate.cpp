c43c8de8e8e989c1b06b65bdd335eccbd71139c4
DesignMoon <enjoymoon@gmail.com>
2024-07-17 01:40:36 +0900

    Update set_terminate.cpp
    독자 의견 반영, 혼동 없도록 주석 추가

--- sample_code/ch05/set_terminate/set_terminate.cpp
+++ sample_code/ch05/set_terminate/set_terminate.cpp
@@ -1,18 +1,18 @@
 ﻿#include <iostream>
-#include <exception>  
+#include <cstdlib>  // exit와 set_terminate 함수 사용을 위해 추가
 
 using namespace std;
 
 // 종료 처리 함수 
 void myterminate() {
   cout << "myterminate called" << endl;
-  exit(-1);      // 프로그램 종료
+  exit(-1);      // 프로그램을 비정상적으로 종료
 }
 
 int main(void) {
-  set_terminate(myterminate);    // 종료 처리 함수 지정
+  set_terminate(myterminate);    // 종료 처리 함수를 myterminate로 지정
 
-  throw 1;    // 처리되지 않는 예외 발생
+  throw 1;    // 처리되지 않는 예외를 던짐
 
-  return 0;
+  return 0;   // 이 줄은 실행되지 않음, 위에서 예외가 던져졌기 때문
 }
